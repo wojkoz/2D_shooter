84731be9c8189ab18269d772cae192618c597ab7
TheEnter3 <wkoziol3ib@gmail.com>
2019-03-15 18:33:56 +0100

    Dodaj pliki projektów.

---
+++ 2D_shooter/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(800, 800), "SFML works!");
+	sf::CircleShape shape(300.f);
+	shape.setFillColor(sf::Color::Red);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
